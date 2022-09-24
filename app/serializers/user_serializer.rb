ab9282f289b4f493ab5c4ae6f259dd25d665f8b6
jayastronomic <61813086+jayastronomic@users.noreply.github.com>
2022-09-24 11:55:19 -0500

    added sessions

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,26 @@
 class UserSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
-    attributes :email, :name, :bio, :age, :birth_date, :logged_in
+    attributes :id, :email, :name, :bio, :age, :birth_date, :logged_in, :avatar_exist, :avatar_url
 
     def logged_in
         @instance_options[:logged_in]
+    end
+
+    def avatar_exist
+        object.avatar.attached?
+    end
+
+    def avatar_url
+        if avatar_exist then url_for(object.avatar) else false end
+    end
+
+    def images_exist
+        object.images.attached? 
+    end
+
+    def images
+        object.images.map do |image|
+            url_for(image)
+        end
     end
 end
